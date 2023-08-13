fcb94d8658f0ac3f4af895518168726a3e3d987e
Nwosu Chukwuemeka K <78231106+Kachu1212@users.noreply.github.com>
2023-08-13 07:12:55 +0100

    Updated dashboard_id_locator.py

--- 0x18-webstack_monitoring/dashboard_id_locator.py
+++ 0x18-webstack_monitoring/dashboard_id_locator.py
@@ -3,10 +3,10 @@ import requests
 
 # Set up the API endpoint and parameters
 api_url = "https://api.datadoghq.com/api/v1/dashboard/"
-api_key = "d4856644351b156fbc9057d09a3eba64"
-api_app_key = "c08448f2f2311e14821accfd4c45010e7266b2f2"
+api_key = "........"
+api_app_key = "........"
 headers = {'Content-type': 'application/json', 'DD-API-KEY': api_key, 'DD-APPLICATION-KEY': api_app_key}
-query_params = {'filter': 'host:187813-web-01'}
+query_params = {'filter': 'host: 183813-web-01'}
 
 # Make the API call to get the host details
 response = requests.get(api_url, headers=headers, params=query_params)
